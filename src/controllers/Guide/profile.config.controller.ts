fc754f719ed8e7497af193e0316fa242835d98d4
andres06 <andressierrarojas7@gmail.com>
2022-05-03 07:35:06 -0500

    Se termina el Update de datos de tipo guia SOLO en MyQSL

--- src/controllers/Guide/profile.config.controller.ts
+++ src/controllers/Guide/profile.config.controller.ts
@@ -4,23 +4,27 @@ import { Request, Response } from 'express';
 /////////////////////////////////////////
 // IMPORTAMOS SERVICIOS
 import { updateData } from '../../services/update.service';
+import { pullApartToken, verifyToken } from '../../services/token.service';
 ////////////////////////////////////////
 //IMPORTAMOS INTERFACES
 import { GuideSignup_extra } from '../../interface/Guide/signup-guide.extra';
-/////////////////////////////////////////////////////
+import { Payload } from '../../interface/payload-token';
+////////////////////////////////////////////////////////////////
 
 //->> RUTA POST
 //Validar datos de entrada
 export const profileConfig_put = async (req:Request, res:Response) => { 
-	//Obtenemos los parametros por ruta
-	const { id } = req.params;
 	//Obtenemos cabecera
-	const { authorization } = req.headers;
-	console.log('\nDATA FOR HEADERS :: ', authorization, '\n');
-	//TODO -> VALIDAR LOS ID DEL TOKEN Y PARAMS 
-	// Get Data to configure por rl 'Body'
+	// const { authorization } = req.headers;
+	const authorization : string | undefined = req.headers.authorization;
+	//Obtenemos el ID del Headers
+	const id : number = await getId(authorization);
+	console.log('-> ID payload :: ', id);
+	if(id == 0) return res.status(500).json({ error:'Unauthorized' });
+	
+	// Get Data to configure for the 'Body'
 	const { firstName, lastName, dataOfBirth, city, phoneNumber, information } = req.body as GuideSignup_extra;
-	console.log('Id user to update :: ',id);
+	// console.log('Id user to update :: ',id);
 	//Save or update data in MySQL and MongoDB
 	try {
 		//TODO -> ORGANIZAR LAS FUNCIONES A LLAMAR
@@ -45,3 +49,26 @@ export const profileConfig_put = async (req:Request, res:Response) => {
 		res.status(500).json({ msg : 'Error :: ' });
 	}
 };
+
+//Funcion solo para obtener el token
+async function getToken(authorization ?: string) : Promise<string>{
+	//VAlidamos de que si llegue
+	if (!authorization) return '';
+	//Decodificamos cabecera para asi obtener el id
+	const token : string = await pullApartToken( authorization );
+	return token;
+}
+
+//Obtenemos el ID que nos da el token en el payload
+async function getId( authorization ?: string) : Promise<number> {
+	//Validamos el token
+	if (!authorization) return 0;
+	//Obtenemos el token
+	const token : string = await getToken( authorization );
+	//Decode token
+	const decodeToken = await verifyToken(token) as Payload;
+	//Data token
+	//Decosntruccion, {id}
+	const { id } = decodeToken;
+	return id;
+}
--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -20,17 +20,17 @@ export const loginPost = async (req:Request, res:Response) => {
 	const { email, password } = req.body as User;
 	try{
 		// obtenemos el token
-		const token : string | boolean = await authSrv.login(email, password);
+		const result : object | boolean = await authSrv.login(email, password);
 		// si retorna el toquen
 		// Si nos retorna un false es porque ocurrio un error
-		if(token === false){
+		if(result === false){
 			// Respondemos al cliente
 			return res.status(401).json({msg:'Invalid Credentials'});
 			// Por lo contrario repondemos
 		}else{
 			// console.log('token: ',token);
 			return res.status(200).json({
-				token
+				result
 			});
 		}
 	}catch(e){// Si nos devuelve un error
