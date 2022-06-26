367b76517beb247a14508973e4b5328f1255c297
andres06 <andressierrarojas7@gmail.com>
2022-06-25 20:17:47 -0500

    ADD: URL mongo

--- src/config/connection/MongoDB/conection-mongodg.ts
+++ src/config/connection/MongoDB/conection-mongodg.ts
@@ -7,13 +7,15 @@ const port = process.env.MONGO_PORT;
 const host = process.env.MONGO_HOST;
 const password = process.env.MONGO_PASSWORD;
 const username = process.env.MONGO_USERNAME;
+const URL = `mongodb://${username}:${password}@${host}:${port}/${noSql_db}`;
 // console.log('mongodb://'+host+':'+port+'/'+noSql_db);
 console.log(`mongodb://${username}:${password}@${host}:${port}/${noSql_db}`);
+
 //Coneccion
 export async function connectDB() {
 	console.log('CONECTANDO...');
 	// const db = await connect('mongodb://'+host+':'+port+'/'+noSql_db);
-	const db = await connect(`mongodb://${username}:${password}@${host}:${port}/${noSql_db}`);
+	const db = await connect(URL);
 	// const db = await connect(host+':'+port+'/'+noSql_db);
 	console.log('Mongo:DataBase is connected to :: ', db.connection.db.databaseName);
 }
